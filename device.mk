042482972413d07a6bba4af0a7d98a91b6ec8861
kunjavo <114494546+kunjavo@users.noreply.github.com>
2022-10-02 09:13:29 +0530

    Typo

--- device.mk
+++ device.mk
@@ -28,8 +28,8 @@ PRODUCT_COPY_FILES := \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
 	$(DEVICE_PATH)/overlay \
-	$(DEVICE_PATH)/overlay-lineage
-       $(DEVICE_PATH)/overlay-xd
+	$(DEVICE_PATH)/overlay-lineage \
+        $(DEVICE_PATH)/overlay-xd
 
 # Dependencies of kpoc_charger
 PRODUCT_PACKAGES += \
